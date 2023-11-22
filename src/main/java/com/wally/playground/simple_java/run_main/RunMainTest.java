347d166f9deb1a2fea82b0e6384d5e5149b58287
wally <goldenale5500@gmail.com>
2023-11-22 15:45:49 +0900

    main 메소드 실행 테스트 추가

---
+++ src/main/java/com/wally/playground/simple_java/run_main/RunMainTest.java
@@ -0,0 +1,8 @@
+package com.wally.playground.simple_java.run_main;
+
+public class RunMainTest {
+
+    public static void main(String[] args) {
+        TargetClass.main(new String[]{"arg1", "arg2", "arg3"});
+    }
+}
---
+++ src/main/java/com/wally/playground/simple_java/run_main/TargetClass.java
@@ -0,0 +1,9 @@
+package com.wally.playground.simple_java.run_main;
+
+public class TargetClass {
+
+    public static void main(String[] args) {
+        for (String s : args)
+            System.out.println(s);
+    }
+}
