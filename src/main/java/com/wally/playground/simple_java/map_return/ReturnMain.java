eff449db067bd5edfce0c3e010e43f69b382a9c0
wally <goldenale5500@gmail.com>
2024-02-20 01:47:18 +0900

    map 리턴 샘플 추가

---
+++ src/main/java/com/wally/playground/simple_java/map_return/ReturnMain.java
@@ -0,0 +1,29 @@
+package com.wally.playground.simple_java.map_return;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReturnMain {
+    public static void main(String[] args) {
+        Map<String, String> map = getMapV1();
+        System.out.println("class name is " + map.getClass().getName());
+
+        Map<String, String> map2 = getMapV2();
+        System.out.println("class name is " + map2.getClass().getName());
+    }
+
+    public static Map<String, String> getMapV1() {
+        return new HashMap<>() {{
+            put("key1", "value1");
+            put("key2", "value2");
+        }};
+    }
+
+    public static Map<String, String> getMapV2() {
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+
+        return map;
+    }
+}
